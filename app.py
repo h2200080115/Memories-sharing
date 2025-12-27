adcc0e2b32e7201601837ffdf79c4643bc078b7b
karthik3468 <2200080115@kluniversity.in>
2025-12-27 11:02:04 +0530

    Fix password hash length issue

--- app.py
+++ app.py
@@ -48,10 +48,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     mobile = db.Column(db.String(20), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
